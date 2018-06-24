12299de442301296dcdf4c8fd2d256df4eefe72d
zongyuanZhan <1450303552@qq.com>
2018-06-24 12:50:59 +0800

    Fix routes bug.

--- routes.js
+++ routes.js
@@ -44,7 +44,7 @@ router.delete('/session', sessionController.deleteSession)
 router.get('/restaurant', restaurantController.getRestaurant)
 router.post('/restaurant', restaurantController.registerRestaurant)
 router.put('/restaurant/desk', restaurantController.updateDesk)
-router.route('/menu/dish').put(upload.single('avatar'), restaurantController.updateRestaurant)
+router.route('/restaurant').put(upload.single('avatar'), restaurantController.updateRestaurant)
 
 // menu API
 router.get('/menu', menuController.getMenu)
@@ -58,3 +58,4 @@ router.post('/order', orderController.createOrder)
 router.get('/order', orderController.getOrders)
 router.put('/order', orderController.finishOrders)
 
+module.exports = router
